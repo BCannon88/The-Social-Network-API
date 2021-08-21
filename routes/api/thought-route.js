39f78c2692526d5dc44b9ba106c7828ca0f223a8
Baron C <cannonbaron@gmail.com>
2021-08-21 16:30:27 -0500

    adding api routes

---
+++ routes/api/thought-route.js
@@ -0,0 +1,40 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    removeThought
+} = require('../../controllers/thought-controller');
+
+const { route } = require('./user-route');
+// /api/users
+router
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:userId')
+    .post(addThought);
+
+// /api/user/:id
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+
+router
+    .route('./:userId/:thoughtId')
+    .delete(removeThought);
+
+///api/thoughts/:thoughtId/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction);
+
+
+module.exports = router;
